88ed0ea3d692e99fa471e993b5d97b50094b05ff
811722066 <811722066@qq.com>
2016-07-18 17:34:37 +0800

    json
    返回json数据

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,13 +5,25 @@ use Tutorial\Models\Users;
 
 class IndexController extends Controller
 {
+	private static function printJSON($json){
+		if(!isset($json) || empty($json)){
+			$json = array();
+		}
+		if($_REQUEST['callback']){
+			$callback = $_REQUEST['callback'];
+			exit($callback.'('.json_encode($json).')');
+		}
+		exit(json_encode($json));
+	}
 
 	public function indexAction()
 	{
-		$cacheKey  = 'users_data';
+		/*$cacheKey  = 'users_data';
 
 		$user = Users::findFirst(1);
-		echo $user->name;
+		echo $user->name;*/
+		$json = 'hello world!';
+		self::printJSON($json);
 		//$this->view->cache();
 
 
